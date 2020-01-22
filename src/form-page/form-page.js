ae3372bbc7a669a269bd9c4493c790a6e6982ddf
Barnicle <egalimov95@gmail.com>
2020-01-23 00:23:04 +0300

    added class Form and babel plugin

--- src/form-page/form-page.js
+++ src/form-page/form-page.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import './form-page.scss'
-const Form = () => {
-  return (
-    <div className="form-container">
-      <h2>Lorem ipsum dolor</h2>
-      <input type="text" placeholder="Ваш телефон" />
-      <button className="form-btn" type="submit">
-        Отправить
-      </button>
-      <div className="checkbox">
-        <input className="" type="checkbox" />
-        <p>Я принимаю условия</p>
-      </div>
-
-      <div className="info">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod
-      </div>
-    </div>
-  );
+import React, { Component } from "react";
+import "./form-page.scss";
+const state = {
+  phone_number: "123"
 };
-export default Form;
+export default class Form extends Component {
+  render() {
+    const data = this.state;
+    return (
+      <div className="form-container">
+        <h2>Lorem ipsum dolor</h2>
+        <input type="text" placeholder="Ваш телефон" />
+        <button className="form-btn" type="submit">
+          Отправить
+        </button>
+        <div className="checkbox">
+          <input className="" type="checkbox" />
+          <p>Я принимаю условия</p>
+        </div>
+        <div className="info">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit, sed do eiusmod
+        </div>
+      </div>
+    );
+  }
+}
